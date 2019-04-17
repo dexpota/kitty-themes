bcf8ccc2a7875e39312f24d94f83229c05a77a85
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-04-17 12:56:16 +0200

    Fixes and docs to the script for generation of previews.

--- .tools/generate_themes_previews.sh
+++ .tools/generate_themes_previews.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
+# This script generate all preview images for the themes
 
-id=$(kitty @ new-window --title themes --window-type os --cwd ~/developing/projects/kitty-themes/.tools)
+root="$(git rev-parse --show-toplevel)"
+tools="$root/.tools"
+
+# new kitty window, return its id
+id=$(kitty @ new-window --title themes --window-type os --cwd "$tools")
+# get system id of the new created window
 sys_id=$(./windowid.swift "kitty" "themes")
+# start bash without reading the profile nor the configuration
 kitty @ send-text --match id:$id "/usr/local/bin/bash --noprofile --norc\n"
-exit
-find ../themes -name "*.conf" -print0 | while read -d $'\0' -r theme; do	
+
+# save all preview in this directory
+previews="$root/_previews"
+mkdir "$previews"
+
+find "$root/themes" -name "*.conf" -print0 | while read -d $'\0' -r theme; do
 	preview_filename=$(basename ${theme%.*})
 	kitty @ set-colors --match id:$id "$theme"
 	kitty @ send-text --match id:$id "clear\n"
 	kitty @ send-text --match id:$id "./color_table.sh && echo -e '\\e[1S'\n"
-	sleep 1
-	screencapture -l$sys_id ./previews/$preview_filename.png
+	screencapture -l$sys_id "$previews/$preview_filename.png"
 done
