bc2449e256ea2159ae74e596a4a29cab4b36f26e
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-06-18 22:25:00 +0200

    Updated script to generate markdown README file.

--- .tools/markdown.sh
+++ .tools/markdown.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-for f in $(find ../previews -name "*.png" | sort); do
+previews_root=$1
+
+for f in $(find "$previews_root" -maxdepth 1 -type d | sort); do
+	preview_file="$f"/preview.png
+	echo $preview_file
+	relative_path=$(realpath --relative-to="$previews_root" "$preview_file")/previews
 	header=`basename ${f%.*} | sed 's/_/ /g'`
 	image="![image]($f)"
 	echo \#\# $header
