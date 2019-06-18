66463e7a669e90773109843bce39c04f8ced628b
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-06-18 14:46:50 +0200

    Updated tools scripts.

--- .tools/generate_theme_preview.sh
+++ .tools/generate_theme_preview.sh
@@ -3,48 +3,18 @@
 # In order to capture previews without the to bar start kitty without decorations
 # kitty -o hide_window_decorations=yes
 
-
-function capture_linux() {
-	local title="$1"
-	local output="$2"
-	import -frame -window "$title" "$output"
-}
-
-function capture_osx() {
-	local title="$1"
-	local output="$2"
-	# get system id of the new created window
-	sys_id=$(./windowid.swift "kitty" "$title")
-	screencapture -wl$sys_id "$output"
-}
-
-
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        capture="capture_linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	capture="capture_osx"
-fi
-
-
-root="$(git rev-parse --show-toplevel)"
-tools="$root/.tools"
-
-# new kitty window, return its id
-id=$(kitty @ new-window --title themes --window-type os --cwd "$tools")
-# start bash without reading the profile nor the configuration
-kitty @ send-text --match id:$id "/usr/bin/env bash --noprofile --norc\n"
-
-# save all preview in this directory
-previews="$root/_previews"
-if [ ! -d "$previews" ]; then
-	mkdir "$previews"
-fi
+# shellcheck source=libcapture.sh
+source libcapture.sh
 
 # read theme path from args
-theme=$1
-lockfile=$(mktemp)
-preview_filename=$(basename ${theme%.*})
-kitty @ set-colors --match id:$id "$theme"
-kitty @ send-text --match id:$id "clear && ./color_table.sh && rm \"$lockfile\"\n"
+id=$1
+theme=$2
+previews=$3
 
-fswatch -1 -e Removed "$lockfile" > /dev/null && $($capture themes "$previews/$preview_filename.png")
+lockfile=$(mktemp)
+preview_filename=$(basename "${theme%.*}")
+kitty @ set-colors --match id:"$id" "$theme"
+kitty @ send-text --match id:"$id" "clear && ./color_table.sh && rm \"$lockfile\"\n"
+
+# simple sync mechanism, wait for the lockfile to be removed
+( echo "$lockfile" | entr "false" 1>/dev/null 2>&1 ) || capture themes "$previews/$preview_filename.png"
--- .tools/generate_themes_previews.sh
+++ .tools/generate_themes_previews.sh
@@ -3,21 +3,21 @@
 
 root="$(git rev-parse --show-toplevel)"
 tools="$root/.tools"
+themes="$root/themes"
 
 # new kitty window, return its id
 id=$(kitty @ new-window --title themes --window-type os --cwd "$tools")
-# get system id of the new created window
-sys_id=$(./windowid.swift "kitty" "themes")
 # start bash without reading the profile nor the configuration
-kitty @ send-text --match id:$id "/usr/local/bin/bash --noprofile --norc\n"
+kitty @ send-text --match id:"$id" "/usr/bin/env bash --noprofile --norc\n"
 
 # save all preview in this directory
 previews="$root/_previews"
-mkdir "$previews"
+if [ ! -d "$previews" ]; then
+	mkdir "$previews"
+fi
 
-find "$root/themes" -name "*.conf" -print0 | while read -d $'\0' -r theme; do
-	preview_filename=$(basename ${theme%.*})
-	kitty @ set-colors --match id:$id "$theme"
-	kitty @ send-text --match id:$id "clear && ./color_table.sh\n"
-	screencapture -l$sys_id "$previews/$preview_filename.png"
+
+find "$themes" -name "*.conf" -print0 | while read -d $'\0' -r theme; do
+	echo "Genereting theme preview for $theme"
+	./generate_theme_preview.sh "$id" "$theme" "$previews"
 done
