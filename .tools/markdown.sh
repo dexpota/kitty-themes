3bdda97664919fa3b03ee1def926d5c7b133368f
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-06-19 07:59:38 +0200

    Updated script to generate previews' markdown.

--- .tools/markdown.sh
+++ .tools/markdown.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 previews_root=$1
+# usually this value: https://raw.githubusercontent.com/dexpota/kitty-themes-website/master
+url_root=$2
 
-for f in $(find "$previews_root" -maxdepth 1 -type d | sort); do
+for f in $(find "$previews_root/previews" -maxdepth 1 -mindepth 1 -type d | sort); do
 	preview_file="$f"/preview.png
-	echo $preview_file
-	relative_path=$(realpath --relative-to="$previews_root" "$preview_file")/previews
-	header=`basename ${f%.*} | sed 's/_/ /g'`
-	image="![image]($f)"
+	theme=$(basename $f)
+	relative_path=$(realpath --relative-to="$previews_root" "$preview_file")
+	header=`basename $theme | sed 's/_/ /g'`
+	image="![image]($url_root/$relative_path)"
 	echo \#\# $header
 	echo $image
 done
