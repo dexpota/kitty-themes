7e6138426ebb005d3f2bca60c512aaa66e1a8ad7
Fabrizio Destro <destro.fabrizio@gmail.com>
2019-07-07 17:58:42 +0200

    Updated script.

--- .tools/generate_themes_previews.sh
+++ .tools/generate_themes_previews.sh
@@ -3,7 +3,8 @@
 
 root="$(git rev-parse --show-toplevel)"
 tools="$root/.tools"
-themes="$root/monokai"
+
+PATH=$tools:$PATH
 
 # new kitty window, return its id
 id=$(kitty @ new-window --title themes --window-type os --cwd "$tools")
@@ -14,18 +15,18 @@ kitty @ set-font-size 24
 # save all preview in this directory
 previews="$root/_previews"
 if [ ! -d "$previews" ]; then
-	mkdir "$previews"
+  mkdir "$previews"
 fi
 
-
-find "$themes" -name "*.conf" -print0 | while read -d $'\0' -r theme; do
-	echo "Genereting theme preview for $theme"
-	preview_directory=$previews/$(basename "${theme%.*}")
-	[ ! -d "$preview_directory" ] && mkdir "$preview_directory"
-	preview_filename=$previews/$(basename "${theme%.*}")/preview.png
-	./generate_theme_preview.sh "$id" "$theme" "$preview_filename"
-	mogrify -resize 1024x\> "$preview_filename"
-done
+while read -r theme
+do
+  echo "Genereting theme preview for $theme"
+  preview_directory=$previews/$(basename "${theme%.*}")
+  [ ! -d "$preview_directory" ] && mkdir "$preview_directory"
+  preview_filename=$previews/$(basename "${theme%.*}")/preview.png
+  generate_theme_preview.sh "$id" "$theme" "$preview_filename"
+  mogrify -resize 1024x\> "$preview_filename"
+done < /dev/stdin
 
 kitty @ close-window --match id:"$id"
 kitty @ set-font-size 16
