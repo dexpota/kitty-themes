830c28f2dc77838826e785c8f5f1e73dc4f0746b
Peter Benjamin <petermbenjamin@gmail.com>
2021-10-21 21:50:20 -0700

    feat: port mbadolato/iterm2-color-schemes

--- .tools/generate_conf.sh
+++ .tools/generate_conf.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-find ~/github/macos-terminal-themes/schemes/ -name "*.terminal" -print0 | while read -d $'\0' -r file; do 
+find /Users/pbenjamin/Projects/github.com/mbadolato/iTerm2-Color-Schemes/terminal -name "*.terminal" -print0 | while read -d $'\0' -r file; do
 	filename=`basename "$file"`
 	without_ext=${filename%.*}
 	removed_spaces=${without_ext// /_}
 	# output_filename=`echo ${removed_spaces} | tr '[:upper:]' '[:lower:]'`
 	echo ${removed_spaces}.conf
-	./convert_conf.swift "$file" > ./themes/${removed_spaces}.conf
+	./.tools/convert_conf.swift "$file" > ./themes/${removed_spaces}.conf
 done
